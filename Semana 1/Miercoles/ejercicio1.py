6972228ff8a84d43674335f1b768c5bface2dac1
daniel-morillo <daniel.morillo@correo.unimet.edu.ve>
2022-09-16 11:09:16 -0400

    Ejercicio 1

---
+++ Semana 1/Miercoles/ejercicio1.py
@@ -0,0 +1,5 @@
+from os import name
+
+
+name = input("What is your name?")
+print(f"Hola {name}")
---
+++ Semana 1/Miercoles/ejercicio2.py
@@ -0,0 +1,4 @@
+horas_trabajadas = int(input("Cuántas horas trabajaste este mes"))
+salario_por_hora = int(input("Cuánto cobras por hora"))
+salario_total= horas_trabajadas * salario_por_hora
+print(f"Tu paga total es {salario_total}")
